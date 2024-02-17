d88e8420a12fe3a461529cfba1135f74f338bbb3
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-02-17 18:02:06 +0530

    rows created

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -10,14 +10,113 @@ const MainPage = () => {
 
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          margin: "20px",
-        }}
-      >
+      {/*row 1*/}
+      <div className="row">
+        {/* Button at the top-left corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C1" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C1")}
+        >
+          R1C1
+        </button>
+
+        {/* Button at the top-center position */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C2" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C2")}
+        >
+          R1C2
+        </button>
+
+        {/* Button at the top-right corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C3" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C3")}
+        >
+          R1C3
+        </button>
+      </div>
+      {/*row 2*/}
+      <div className="row">
+        {/* Button at the top-left corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C1" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C1")}
+        >
+          R1C1
+        </button>
+
+        {/* Button at the top-center position */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C2" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C2")}
+        >
+          R1C2
+        </button>
+
+        {/* Button at the top-right corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C3" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C3")}
+        >
+          R1C3
+        </button>
+      </div>
+      {/*row 3*/}
+      <div className="row">
+        {/* Button at the top-left corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C1" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C1")}
+        >
+          R1C1
+        </button>
+
+        {/* Button at the top-center position */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C2" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C2")}
+        >
+          R1C2
+        </button>
+
+        {/* Button at the top-right corner */}
+        <button
+          style={{
+            backgroundColor: selectedButton === "R1C3" ? "green" : "gray",
+            margin: "5px",
+          }}
+          onClick={() => handleButtonClick("R1C3")}
+        >
+          R1C3
+        </button>
+      </div>
+      {/*row 4*/}
+      <div className="row">
         {/* Button at the top-left corner */}
         <button
           style={{
